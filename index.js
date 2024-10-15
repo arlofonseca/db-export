2faf45aee5d843a6b323fca7a40849f2c559ccd2
arlofonseca <lj6976294@mail.com>
2024-10-14 22:48:50 -0400

    feat: send log to discord

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 require('dotenv').config()
 
+const axios = require('axios');
 const cron = require('node-cron')
 const path = require('path')
 const fs = require('fs')
 const { exec } = require('child_process')
 
 const db = path.resolve(__dirname, 'db')
-const { USER: DB_USER, PASSWORD: DB_PASSWORD, NAME: DB_NAME } = process.env
+const { USER: DB_USER, PASSWORD: DB_PASSWORD, NAME: DB_NAME, LOG_CHANNEL: WEBHOOK } = process.env
 
-if (!DB_USER || !DB_PASSWORD || !DB_NAME) {
+if (!DB_USER || !DB_PASSWORD || !DB_NAME || !WEBHOOK) {
   console.error('Environment variables missing.')
   process.exit(1)
 }
 
 if (!fs.existsSync(db)) {
   fs.mkdirSync(db)
+}
+
+const sendDiscordWebhookMessage = async (title, description, color) => {
+  try {
+    await axios.post(WEBHOOK, {
+      embeds: [
+        {
+          title: title,
+          description: description,
+          color: color,
+        },
+      ],
+    })
+  } catch (error) {
+    console.error('Error sending message:', error)
+  }
+}
+
+const generateRandomNumber = (length) => {
+  const min = Math.pow(10, length - 1);
+  const max = Math.pow(10, length) - 1;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const execute = (command) => {
@@ -31,14 +54,17 @@ const execute = (command) => {
 
 const initBackup = async () => {
   const timestamp = new Date().toISOString().replace(/[:.]/g, '-')
+  const vim = generateRandomNumber(4);
   const file = path.join(db, `${DB_NAME}_backup_${timestamp}.sql`)
   const command = `mariadb-dump --user=${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} > "${file}"`
 
   try {
     await execute(command)
     console.log(`Backup created successfully: ${file}`)
+    await sendDiscordWebhookMessage('Database Backup', `\`🟢\` Backup created (g${vim})`, 0x008000);
   } catch (error) {
     console.error('Error taking backup:', error.message)
+    await sendDiscordWebhookMessage('Database Backup', `\`🔴\` Backup failed (g${vim})`, 0x800000);
   }
 }
 
